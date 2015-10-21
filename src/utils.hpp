bc5106fa0b8bb3e4fad5cd85f59797561777bda2
stryku <stryku2393@gmail.com>
2015-10-22 01:26:03 +0200

    created UNKNOW hotkey methods hotkeyToStdString stdStringToHotkey

--- src/utils.hpp
+++ src/utils.hpp
@@ -23,8 +23,49 @@ namespace AMB
             F9,
             F10,
             F11,
-            F12
+            F12,
+
+            UNKNOWN
         };
+
+        std::string hotkeyToStdString( Hotkey hot )
+        {
+            switch( hotkey )
+            {
+                case Utils::Hotkey::F1: return "F1";
+                case Utils::Hotkey::F2: return "F2";
+                case Utils::Hotkey::F3: return "F3";
+                case Utils::Hotkey::F4: return "F4";
+                case Utils::Hotkey::F5: return "F5";
+                case Utils::Hotkey::F6: return "F6";
+                case Utils::Hotkey::F7: return "F7";
+                case Utils::Hotkey::F8: return "F8";
+                case Utils::Hotkey::F9: return "F9";
+                case Utils::Hotkey::F10: return "F10";
+                case Utils::Hotkey::F11: return "F11";
+                case Utils::Hotkey::F12: return "F12";
+
+                default: return "UNKNOW";
+            }
+        }
+
+        Hotkey stdStringToHotkey( const std::string &str )
+        {
+            if( str == "F1" ) return Hotkey::F1;
+            if( str == "F2" ) return Hotkey::F2;
+            if( str == "F3" ) return Hotkey::F3;
+            if( str == "F4" ) return Hotkey::F4;
+            if( str == "F5" ) return Hotkey::F5;
+            if( str == "F6" ) return Hotkey::F6;
+            if( str == "F7" ) return Hotkey::F7;
+            if( str == "F8" ) return Hotkey::F8;
+            if( str == "F9" ) return Hotkey::F9;
+            if( str == "F10" ) return Hotkey::F10;
+            if( str == "F11" ) return Hotkey::F11;
+            if( str == "F12" ) return Hotkey::F12;
+
+            return Hotkey::UNKNOWN;
+        }
 
         struct RandomBetween
         {
