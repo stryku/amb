604a53dcefb6dbf5fe2c5f970d5bcf587bc257fe
stryku <stryku2393@gmail.com>
2016-11-19 01:12:40 +0200

    created health and mana bars configs

---
+++ src/config/layout/health_mana_bars/HealthManaBars.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "utils/Structs.hpp"
+
+namespace Amb
+{
+    namespace Config
+    {
+        namespace Layout
+        {
+            namespace HealthManaBars
+            {
+                namespace Static
+                {
+                    namespace HealthBar
+                    {
+                        static constexpr size_t healthBarOffsetFromHeartY_10 = 17;
+                        static constexpr size_t healthBarOffsetFromHeartY_11 = 18;
+
+                        static constexpr AMB::Rgba firstPixel{ 79, 79, 211, 255 };
+                        static constexpr AMB::Rgba otherPixels{ 79, 79, 219, 255 };
+                    }
+
+                    namespace ManaBar
+                    {
+                        static constexpr size_t manaBarOffsetFromHeartY_10 = 4;
+                        static constexpr size_t manaBarOffsetFromHeartY_11 = 5;
+
+                        static constexpr AMB::Rgba firstPixel{ 211, 79, 82, 255 };
+                        static constexpr AMB::Rgba otherPixels{ 218, 80, 83, 255 };
+                    };
+
+                    static constexpr size_t maxLength = 87;
+                    static constexpr size_t offsetFromHeartX = 17;
+                }
+
+                struct BarConfig
+                {
+                    AMB::Rgba firstPixel;
+                    AMB::Rgba otherPixels;
+                    AMB::Pos offsetFromHeart;
+                    size_t maxLength;
+                };
+
+                struct BarsConfig
+                {
+                    BarConfig health;
+                    BarConfig mana;
+                };
+            }
+        }
+    }
+
+}
--- src/tibiascreenreader.hpp
+++ src/tibiascreenreader.hpp
@@ -3,6 +3,9 @@
 #include "screencapturer.hpp"
 #include "utils/Structs.hpp"
 #include "config/layout/health_heart/HealthHeartConfig.hpp"
+#include "config/layout/health_mana_bars/HealthManaBarsConfigFactory.hpp"
+#include "config/layout/health_mana_bars/HealthManaBars.hpp"
+
 #include "graphics/Image.hpp"
 
 #include <QtDebug>
@@ -20,16 +23,17 @@ namespace AMB
                                   Amb::Config::Layout::HealthHeart::HealthHeartConfig &heartConfig)
                     : screen{ screen }
                     , heartConfig{ heartConfig }
+                    , barsConfig{ Amb::Config::Layout::HealthManaBars::Factory{}.create(Amb::Client::TibiaClientType::Tibia10) }
                 {}
 
                 size_t getHpPercent() const
                 {
-                    return getPercent(hpInfo);
+                    return getPercent(barsConfig.health);
                 }
 
                 size_t getManaPercent() const
                 {
-                    return getPercent(manaInfo);
+                    return getPercent(barsConfig.mana);
                 }
 
                 size_t isHearthVisible() const
@@ -64,7 +68,7 @@ namespace AMB
                 }
 
             private:
-                struct HpManaInfo
+                /*struct HpManaInfo
                 {
                     Rgba firstPixel;
                     Rgba otherPixels;
@@ -72,29 +76,30 @@ namespace AMB
 
                     static constexpr size_t MaxLength = 87;
                     static constexpr size_t OffsetFromHeartX = 17;
-                };
+                };*/
 
-                size_t getPercent(const HpManaInfo &info) const
+                size_t getPercent(const Amb::Config::Layout::HealthManaBars::BarConfig &config) const
                 {
                     size_t px{ 0 };
-                    const Pos base{ info.OffsetFromHeartX + screen.w - heartConfig.offsetFromRight - 1,
-                                    info.offsetFromHeartY + heartConfig.y };
+                    const Pos base{ config.offsetFromHeart.x + screen.w - heartConfig.offsetFromRight - 1,
+                        config.offsetFromHeart.y + heartConfig.y };
 
-                    if (screen.cpixel(base.x, base.y) != info.firstPixel)
+                    if (screen.cpixel(base.x, base.y) != config.firstPixel)
                         return 0;
 
                     ++px;
 
-                    while (screen.cpixel(base.x + px, base.y) == info.otherPixels && px < HpManaInfo::MaxLength)
+                    while (screen.cpixel(base.x + px, base.y) == config.otherPixels && px < config.maxLength)
                         ++px;
 
                     return px;
                 }
 
-                static constexpr HpManaInfo hpInfo = { Rgba{ 79, 79, 211, 255 }, Rgba{ 79, 79, 219, 255 }, 5 };
-                static constexpr HpManaInfo manaInfo = { Rgba{ 211, 79, 82, 255 }, Rgba{ 218, 80, 83, 255 }, 18 };
+                //static constexpr HpManaInfo hpInfo = { Rgba{ 79, 79, 211, 255 }, Rgba{ 79, 79, 219, 255 }, 5 };
+                //static constexpr HpManaInfo manaInfo = { Rgba{ 211, 79, 82, 255 }, Rgba{ 218, 80, 83, 255 }, 18 };
                 const Graphics::Image &screen;
                 Amb::Config::Layout::HealthHeart::HealthHeartConfig &heartConfig;
+                Amb::Config::Layout::HealthManaBars::BarsConfig barsConfig;
             };
         }
     }
