c4a8e3538819b7e1f94d5a3418c14cb21a0c0370
stryku <stryku2393@gmail.com>
2016-11-21 22:25:29 +0200

    created Module class

---
+++ src/module/Module.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <memory>
+
+namespace Amb
+{
+    namespace Module
+    {
+        class ModuleCore;
+        class ModuleUiRemover;
+
+        class Module
+        {
+        public:
+            virtual ~Module() {}
+
+            void run();
+            void hideUi();
+
+        private:
+            std::unique_ptr<ModuleCore> moduleCore;
+            std::unique_ptr<ModuleUiRemover> moduleUiRemover;
+        };
+    }
+}
