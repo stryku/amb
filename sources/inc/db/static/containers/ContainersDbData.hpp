c06f2cdf17916049ae7b0a5b0723bb526cd48d6b
stryku <stryku2393@gmail.com>
2017-01-03 23:53:57 +0200

    regenerated databases

--- sources/inc/db/static/containers/ContainersDbData.hpp
+++ sources/inc/db/static/containers/ContainersDbData.hpp
@@ -1,17 +1,3 @@
 #pragma once
-
 #include "db/static/containers/ContainersDbItem.hpp"
-
-namespace Amb
-{
-    namespace Db
-    {
-        namespace Static
-        {
-            namespace Containers
-            {
-                extern const std::array<Item, 43> ContainersDb;
-            }
-        }
-    }
-}
+namespace Amb{namespace Db{namespace Static {namespace Containers { extern const std::array<Item, 43> ContainersDb;}}}}
--- sources/inc/db/static/items/ItemsDbData.hpp
+++ sources/inc/db/static/items/ItemsDbData.hpp
@@ -1,17 +1,3 @@
 #pragma once
-
 #include "db/static/items/ItemsDbItem.hpp"
-
-namespace Amb
-{
-    namespace Db
-    {
-        namespace Static
-        {
-            namespace Items
-            {
-                extern const std::array<Item, 1829> ItemsDb;
-            }
-        }
-    }
-}
+namespace Amb{namespace Db{namespace Static {namespace Items { extern const std::array<Item, 1829> ItemsDb;}}}}
