a9f4ce56f3f561f9a64146d344b114baf7934207
stryku <stryku2393@gmail.com>
2017-01-08 18:25:20 +0200

    filled DebugModeChecker with basic checks

--- sources/inc/security/debug/DebugModeChecker.hpp
+++ sources/inc/security/debug/DebugModeChecker.hpp
@@ -23,6 +23,16 @@ namespace Amb
                 constexpr static size_t loopDelayMs = 500;
                 constexpr static auto delayDuration = std::chrono::milliseconds{ loopDelayMs };
 
+                bool isDebugged() const;
+
+                bool checkIsDebuggerPresent() const;
+                bool checkIsDebugged() const;
+                bool checkNtGlobalFlags() const;
+                bool checkHeapFlags() const;
+                bool checkVista() const;
+                bool checkNtQueryInformationProcess() const;
+                bool checkCheckRemoteDebuggerPresent() const;
+
                 Callback debugModeDetectedCallback;
                 Utils::ThreadWorker threadWorker;
             };
--- sources/src/security/debug/DebugModeChecker.cpp
+++ sources/src/security/debug/DebugModeChecker.cpp
@@ -1,5 +1,8 @@
 #include "security/debug/DebugModeChecker.hpp"
 
+#include <Windows.h>
+#include <Winternl.h>
+
 namespace Amb
 {
     namespace Security
@@ -8,13 +11,189 @@ namespace Amb
         {
             DebugModeChecker::DebugModeChecker(Callback debugModeDetectedCallback)
             {
-                threadWorker.start(debugModeDetectedCallback, delayDuration);
+                auto loop = [this, debugModeDetectedCallback]
+                {
+                    if (isDebugged())
+                        debugModeDetectedCallback();
+                };
 
+                threadWorker.start(loop, delayDuration);
             }
 
             DebugModeChecker::~DebugModeChecker()
             {
                 threadWorker.stop();
+            }
+
+            bool DebugModeChecker::isDebugged() const
+            {
+                return
+                    checkIsDebuggerPresent() ||
+                    checkIsDebugged() ||
+                    checkNtGlobalFlags() ||
+                    checkHeapFlags() ||
+                    checkNtQueryInformationProcess() ||
+                    checkCheckRemoteDebuggerPresent();
+            }
+
+            bool DebugModeChecker::checkIsDebuggerPresent() const
+            {
+                return IsDebuggerPresent();
+            }
+
+            bool DebugModeChecker::checkIsDebugged() const
+            {
+                bool ret{ false };
+
+                __asm
+                {
+                    push eax
+                    push ebx
+
+                    jmp doStuff
+
+                raiseDebuggedError:
+                    mov ret, 1
+                    jmp end
+
+                doStuff:
+                    mov eax, fs : [30h]
+                    mov ebx, 0
+                    mov bl, BYTE PTR[eax + 2]
+                    cmp bl, 0
+                    jne raiseDebuggedError
+
+                end:
+                    pop ebx
+                    pop eax
+                }
+
+                return ret;
+            }
+
+            bool DebugModeChecker::checkNtGlobalFlags() const
+            {
+                bool ret{ false };
+
+                __asm
+                {
+                    push eax
+                    push ebx
+
+                    jmp doStuff
+
+                raiseDebuggedError:
+                    mov ret, 1
+                    jmp end
+
+                doStuff:
+                    mov eax, fs:[30h]
+                    mov ebx, 0
+                    mov bl, BYTE PTR[eax + 2]
+                    cmp bl, 0
+                    jne raiseDebuggedError
+
+                end:
+                    pop ebx
+                    pop eax
+                }
+
+                return ret;
+            }
+
+            bool DebugModeChecker::checkHeapFlags() const
+            {
+                bool ret{ false };
+
+                __asm
+                {
+                    push eax
+                    push ebx
+
+                    jmp doStuff
+
+                raiseDebuggedError :
+                    mov ret, 1
+                    jmp end
+
+                doStuff:
+                    mov eax, fs : [30h]
+                    mov eax, [eax + 18h]; process heap
+                    mov eax, [eax + 10h]; heap flags
+                    test eax, eax
+                    jne raiseDebuggedError
+
+                end:
+                    pop ebx
+                    pop eax
+                }
+
+                return ret;
+            }
+
+            bool DebugModeChecker::checkVista() const
+            {
+                bool ret{ false };
+
+                __asm
+                {
+                    push eax
+                    push ebx
+
+                    jmp doStuff
+
+                raiseDebuggedError :
+                    mov ret, 1
+                    jmp end
+
+                doStuff :
+                    call GetVersion
+
+                    cmp al, 6
+                    jne end; this is not a Vista
+
+                    ; push offset _seh
+
+                    mov ebx, fs:[0]
+                    push ebx
+
+                    mov fs : [0], esp
+                    mov eax, fs : [18h]; teb
+                    add eax, 0BFCh
+                    mov ebx, [eax]; pointer to a unicode string
+                    test ebx, ebx; (ntdll.dll, gdi32.dll, ...)
+                    je end; debugger not found
+
+                    sub ebx, eax; the unicode string follows the
+                    sub ebx, 4; pointer
+                    jne end; debugger not found
+
+                    ; debugger detected if it reaches this point
+                    jmp raiseDebuggedError
+
+                end:
+                    pop ebx
+                    pop eax
+                }
+
+                return ret;
+            }
+
+            bool DebugModeChecker::checkNtQueryInformationProcess() const
+            {
+                DWORD pBasicInfo;
+
+                NtQueryInformationProcess(HANDLE(-1), PROCESSINFOCLASS(7), &pBasicInfo, 4, 0);
+
+                return pBasicInfo == -1;
+            }
+
+            bool DebugModeChecker::checkCheckRemoteDebuggerPresent() const
+            {
+                BOOL isDebugged;
+                CheckRemoteDebuggerPresent(HANDLE(-1), &isDebugged);
+
+                return isDebugged == 1;
             }
         }
     }
