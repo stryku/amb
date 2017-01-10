9327b7549f51d1a6081912ccf468cb0cd09dc8b4
stryku <stryku2393@gmail.com>
2017-01-10 18:15:30 +0200

    created basic DeadCreatureWindowFinder and its factory

---
+++ sources/inc/client/window/finder/DeadCreatureWindowFinder.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Amb
+{
+    namespace Client
+    {
+        namespace Window
+        {
+            namespace Finder
+            {
+                class DeadCreatureWindowFinder
+                {};
+            }
+        }
+    }
+}
---
+++ sources/src/client/window/finder/DeadCreatureWindowFinderFactory.cpp
@@ -0,0 +1,19 @@
+#include "client/window/finder/DeadCreatureWindowFinderFactory.hpp"
+#include "client/window/finder/DeadCreatureWindowFinder.hpp"
+
+namespace Amb
+{
+    namespace Client
+    {
+        namespace Window
+        {
+            namespace Finder
+            {
+                DeadCreatureWindowFinder create(TibiaClientType clientType)
+                {
+
+                }
+            }
+        }
+    }
+}
