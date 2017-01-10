6e7ed0e61db2a22892c44994b8d4b7ed6975820b
stryku <stryku2393@gmail.com>
2017-01-10 18:39:51 +0200

    implemented create() method

--- sources/src/client/window/finder/DeadCreatureWindowFinderFactory.cpp
+++ sources/src/client/window/finder/DeadCreatureWindowFinderFactory.cpp
@@ -1,5 +1,7 @@
 #include "client/window/finder/DeadCreatureWindowFinderFactory.hpp"
 #include "client/window/finder/DeadCreatureWindowFinder.hpp"
+#include "capture/ConstPixels.hpp"
+#include "log/log.hpp"
 
 namespace Amb
 {
@@ -11,7 +13,20 @@ namespace Amb
             {
                 DeadCreatureWindowFinder create(TibiaClientType clientType)
                 {
+                    switch (clientType)
+                    {
+                    case Amb::Client::TibiaClientType::Tibia10:
+                        return DeadCreatureWindowFinder{ ConstPixels::Slain, ConstPixels::Dead, ConstPixels::Dead };
 
+                    case Amb::Client::TibiaClientType::Tibia11:
+                        return DeadCreatureWindowFinder{ ConstPixels::Slain_11, ConstPixels::Dead_11, ConstPixels::Remains_11 };
+
+                    case Amb::Client::TibiaClientType::Flash:
+                    default:
+                        LOG_DEBUG("unsupported client type in switch");
+                        BOOST_ASSERT_MSG(false, "unsupported client type in switch");
+                        break;
+                    }
                 }
             }
         }
