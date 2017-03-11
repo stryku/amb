d886ec1e34ac6e0aaec8224069136cd581eca00a
stryku <stryku2393@gmail.com>
2017-03-11 19:28:53 +0100

    introduce HealerConfig files

---
+++ sources/inc/config/module/HealerConfig.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+
+namespace Amb
+{
+    namespace Utils
+    {
+        class PropertyTreeBuilder;
+    }
+
+    namespace Module
+    {
+        namespace Heal
+        {
+            struct HealRule;
+        }
+    }
+
+    namespace Config
+    {
+        namespace Module
+        {
+            struct HealerConfig
+            {
+                void toPropertyTreeBuilder(Utils::PropertyTreeBuilder &builder, const std::string &path = "") const;
+                auto toPtree() const;
+                static HealerConfig fromPtree(boost::property_tree::ptree &tree);
+
+                std::vector<Amb::Module::Heal::HealRule> rules;
+            };
+        }
+    }
+}
---
+++ sources/src/config/module/HealerConfig.cpp
@@ -0,0 +1,53 @@
+#include "config/module/HealerConfig.hpp"
+#include "ui/modules/healer/HealRule.hpp"
+#include "utils/PropertyTreeBuilder.hpp"
+
+namespace Amb
+{
+    namespace Config
+    {
+        namespace Module
+        {
+            void HealerConfig::toPropertyTreeBuilder(Utils::PropertyTreeBuilder &builder, const std::string &path = "") const
+            {
+                if (rules.empty())
+                {
+                    builder.addElement(Utils::PtreeElement<>{path + ".heal_rules", ""});
+                    return;
+                }
+
+                for (const auto& rule : rules)
+                    rule.toPropertyTreeBuilder(builder, path + ".heal_rules.heal_rule");
+            }
+
+            auto HealerConfig::toPtree() const
+            {
+                Utils::PropertyTreeBuilder builder;
+
+                if (rules.empty())
+                    return builder.addElement(Utils::PtreeElement<>{"heal_rules", ""}).buildTree();
+
+                for (const auto& rule : rules)
+                {
+                    auto tree = rule.toPtree();
+                    builder.addTree("heal_rules.heal_rule", tree);
+                }
+
+                return builder.buildTree();
+            }
+
+            HealerConfig HealerConfig::fromPtree(boost::property_tree::ptree &tree)
+            {
+                HealerConfig healer;
+
+                for (auto &child : tree.get_child("heal_rules"))
+                {
+                    const auto rule = Amb::Module::Heal::HealRule::fromPtree(child.second);
+                    healer.rules.emplace_back(rule);
+                }
+
+                return healer;
+            }
+        }
+    }
+}
